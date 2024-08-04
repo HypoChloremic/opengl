c97f5ead53406c792c7a5c32134a546cf7697f60
Ali Rassolie <26204559+HypoChloremic@users.noreply.github.com>
2024-08-05 00:21:23 +0200

    more basics

--- ogltemplate/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CC=g++
-CFLAGS=`pkg-config --cflags glew glfw3`
-LDFLAGS=`pkg-config --libs glew glfw3`
-LDFLAGS="$LDFLAGS -lglut"
-
-$CC main.cpp ./common/loadShader.cpp $CFLAGS $LDFLAGS -o progr
