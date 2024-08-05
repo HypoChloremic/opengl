aee274bb2424f82e72eada0a27cefdb6952e7267
Ali Rassolie <26204559+HypoChloremic@users.noreply.github.com>
2024-08-06 01:07:18 +0200

    added more controls

--- ogltemplate/basic_map/src/controls.cpp
+++ ogltemplate/basic_map/src/controls.cpp
@@ -11,6 +11,7 @@ using namespace glm;
 
 glm::mat4 ViewMatrix;
 glm::mat4 ProjectionMatrix;
+glm::mat4 RotationMatrix = glm::mat4(1.0f);
 
 glm::mat4 getViewMatrix(){
 	return ViewMatrix;
@@ -18,7 +19,9 @@ glm::mat4 getViewMatrix(){
 glm::mat4 getProjectionMatrix(){
 	return ProjectionMatrix;
 }
-
+glm::mat4 getRotationMatrix(){
+	return RotationMatrix;
+}
 
 // Initial position : on +Z
 glm::vec3 position = glm::vec3( 0, 0, 5 ); 
@@ -30,7 +33,7 @@ float verticalAngle = 0.0f;
 float initialFoV = 45.0f;
 
 float speed = 0.1f; // 3 units / second
-float mouseSpeed = 0.005f;
+float mouseSpeed = 1e-5f;
 
 float oLeft = -0.1f;
 float oRight = 0.1f;
@@ -38,6 +41,9 @@ float oBottom = -0.1f;
 float oTop = 0.1f;
 float oNear = 0.01f;
 float oFar = 100.0f;
+float zoom = 2.0f;
+
+float rotation = 0.0f;
 
 // Global variable to store the scroll offset
 static double scrollOffset = 0.0;
@@ -88,13 +94,13 @@ void computeMatricesFromInputs(GLFWwindow* window){
 	// Move forward
 	if (glfwGetKey(window, GLFW_KEY_UP ) == GLFW_PRESS){
 		position += direction * deltaTime * speed;
-		oTop += oTop * deltaTime * speed;
+		oTop -= oTop * deltaTime * speed;
 		oBottom -= oBottom * deltaTime * speed;
 	}
 	// Move backward
 	if (glfwGetKey(window, GLFW_KEY_DOWN ) == GLFW_PRESS){
 		position -= direction * deltaTime * speed;
-		oTop -= oTop * deltaTime * speed;
+		oTop += oTop * deltaTime * speed;
 		oBottom += oBottom * deltaTime * speed;
 	}
 	// Strafe right
@@ -121,8 +127,29 @@ void computeMatricesFromInputs(GLFWwindow* window){
 		oFar  += oFar * deltaTime * speed;
 		std::cout << "oNear " << oNear << "oFar " << oFar << std::endl;
 	}
+	if (glfwGetKey(window, GLFW_KEY_Z ) == GLFW_PRESS){
+		zoom += (zoom * deltaTime * 2.0f) / 14.0f; // 114% zoom in  
+		std::cout << "zoom " << zoom << std::endl;
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_X ) == GLFW_PRESS){
+		zoom -= (zoom * deltaTime * 2.0f) / 14.0f; // 114% zoom in  
+		std::cout << "zoom " << zoom << std::endl;
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_R ) == GLFW_PRESS){
+		RotationMatrix = glm::rotate(RotationMatrix, glm::radians(rotation+=0.01f), glm::vec3(0.0f, 0.0f, 1.0f));
+		if(rotation > 360.0f) {
+			rotation = 0.0f;
+		}
+	}
+
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS){
+		RotationMatrix = glm::rotate(RotationMatrix, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	}
 
 	glfwSetScrollCallback(window, scroll_callback);
+
 	float FoV = initialFoV - 5 * scrollOffset;
 
 	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
@@ -130,13 +157,15 @@ void computeMatricesFromInputs(GLFWwindow* window){
 	// Orthographic projection parameters
 
 	// Projection matrix: Orthographic projection
-	ProjectionMatrix = glm::ortho(oLeft, oRight, oBottom, oTop, oNear, oFar);
+	ProjectionMatrix = glm::ortho(oLeft/zoom, oRight/zoom, oBottom/zoom, oTop/zoom, oNear/zoom, oFar/zoom);
+
 	// Camera matrix
 	ViewMatrix       = glm::lookAt(
 		position,           // Camera is here
 		position+direction, // and looks here : at the same position, plus "direction"
 		up                  // Head is up (set to 0,-1,0 to look upside-down)
 	);
+
 
 	// For the next frame, the "last time" will be "now"
 	lastTime = currentTime;
