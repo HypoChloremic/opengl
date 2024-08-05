637280f1f24f311c8f8b12ce188a5c513e3c94bc
hypochloremic <26204559+HypoChloremic@users.noreply.github.com>
2024-08-05 23:52:44 +0200

    build

--- ogltemplate/basic_map/build.sh
+++ ogltemplate/basic_map/build.sh
@@ -3,11 +3,11 @@
 CC=g++
 CFLAGS=`pkg-config --cflags glew glfw3`
 LDFLAGS=`pkg-config --libs glew glfw3`
-LDFLAGS="$LDFLAGS -lglut"
+LDFLAGS="$LDFLAGS -lglut -Wall"
 
 # Include directory for header files
 INCLUDE_DIR="-I./include"
 
 mkdir -p build
 
-$CC $INCLUDE_DIR ./src/main.cpp ./src/buffering.cpp ./src/file_handling.cpp ./common/loadShader.cpp $CFLAGS $LDFLAGS -o ./build/progr
+$CC $INCLUDE_DIR ./src/main.cpp ./src/controls.cpp ./src/buffering.cpp ./src/file_handling.cpp ./src/cleaning.cpp ./common/loadShader.cpp $CFLAGS $LDFLAGS -o ./build/progr
