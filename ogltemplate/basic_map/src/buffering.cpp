82fe63597678cb66d4c6f07ab2349754d5927354
hypochloremic <26204559+HypoChloremic@users.noreply.github.com>
2024-08-05 23:53:29 +0200

    buffering cpp adaptation

--- ogltemplate/basic_map/src/buffering.cpp
+++ ogltemplate/basic_map/src/buffering.cpp
@@ -1,27 +1,17 @@
 #include "buffering.hpp"
 #include <stdexcept>
+#include <vector>
+#include <GL/glew.h>
 
-void draw_triangle(GLuint* VertexArrayID, GLuint* vertexbuffer){
-  if(VertexArrayID == nullptr){
+void draw_triangle(GLuint* VertexArrayID, GLuint* vertexbuffer, std::vector<GLfloat>& vertexdata){
+  if(VertexArrayID == nullptr || vertexbuffer == nullptr){
     throw std::invalid_argument("vertexarrayid is a nullptr");
-  } else if (vertexbuffer == nullptr) {
-    throw std::invalid_argument("vertexbuffer is a nullptr");
   }
 
   glGenVertexArrays(1, VertexArrayID);
   glBindVertexArray(*VertexArrayID);
 
-  static const GLfloat g_vertex_buffer_data[] = {
-    -1.0f, -1.0f, 0.0f,
-    1.0f, -1.0f, 1.0f,
-    0.0f, 1.0f, 0.0f,
-
-    0.5f, -1.0f, 0.0f,
-    1.0f, -1.0f, 1.0f,
-    0.5f, 1.0f, 0.0f,
-  };
-
   glGenBuffers(1, vertexbuffer);
   glBindBuffer(GL_ARRAY_BUFFER, *vertexbuffer); // generate one buffer and store its name in vertexbuffer var
-  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, vertexdata.size() * sizeof(GLfloat), vertexdata.data(), GL_STATIC_DRAW);
 }
