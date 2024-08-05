7d5f9a61d97f6e4db98a5d3f975726a843af55ce
hypochloremic <26204559+HypoChloremic@users.noreply.github.com>
2024-08-05 23:54:55 +0200

    controls header

---
+++ ogltemplate/basic_map/include/controls.hpp
@@ -0,0 +1,12 @@
+#ifndef CONTROLS_HPP
+#define CONTROLS_HPP
+#include <GLFW/glfw3.h>
+#include <glm/ext/matrix_float4x4.hpp>
+
+void computeMatricesFromInputs(GLFWwindow* window);
+glm::mat4 getViewMatrix();
+glm::mat4 getProjectionMatrix();
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+#endif
